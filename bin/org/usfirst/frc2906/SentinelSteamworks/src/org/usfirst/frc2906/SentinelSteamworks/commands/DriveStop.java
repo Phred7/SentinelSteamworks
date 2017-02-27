53a7d9384df94f1dfab8e702b19ce8e2387dfc75
Phred7 <woward00@gmail.com>
2017-02-26 17:31:57 -0800

    'fixed' sendable chooser

---
+++ bin/org/usfirst/frc2906/SentinelSteamworks/src/org/usfirst/frc2906/SentinelSteamworks/commands/DriveStop.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc2906.SentinelSteamworks.commands;
+
+import org.usfirst.frc2906.SentinelSteamworks.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveStop extends Command {
+
+    public DriveStop() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveTrain.stop();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
